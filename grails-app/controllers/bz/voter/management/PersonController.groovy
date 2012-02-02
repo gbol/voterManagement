d0cdc4c5558fa97ec1628872eb86675b7e645022
Roberto Guerra <uris77@gmail.com>
2012-02-01 21:09:12 -0600

    Fields for excel format of voters list modified. Client requested that the address list be split into the following columns: 1.House # 2.Street 3.Municipality

--- grails-app/controllers/bz/voter/management/PersonController.groovy
+++ grails-app/controllers/bz/voter/management/PersonController.groovy
@@ -10,6 +10,28 @@ class PersonController {
 	def exportService
 	def voterService
 
+    def excelFields = ["registrationDate", "registrationNumber", "lastName", "firstName",
+                        "age", "birthDate", "registrationAddress.houseNumber",
+                        "registrationAddress.street", "registrationAddress.municipality",
+                        "registrationAddress"]
+    
+    def pdfFields = ["registrationDate", "registrationNumber", "lastName", "firstName",
+                        "age", "birthDate", "registrationAddress"]
+
+    def excelLabels = ["registrationDate": "Registration Date", "registrationNumber": "Registration Number",
+                        "firstName": "First Name", "lastName": "Last Name", "birthDate": "DOB",
+                        "age": "Age", "registrationAddress.houseNumber": "House #",
+                        "registrationAddress.street": "Street", "registrationAddress.municipality": "municipality",
+                        "registrationAddress": "Registration Address"]
+
+    def pdfLabels = ["registrationDate": "Registration Date", "registrationNumber": "Registration Number",
+                    "firstName": "First Name", "lastName": "Last Name", "birthDate": "DOB",
+                    "age": "Age", "registrationAddress": "Registration Address"]
+
+
+    def pdfParams = ["column.widths": [0.1, 0.1, 0.2, 0.2, 0.1, 0.1, 0.2]]
+    def excelParams = ["column.widths": [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]]
+
 
 	 def list = {
 	 	if(params?.format && params.format != 'html'){
@@ -17,12 +39,24 @@ class PersonController {
 			response.contentType = ConfigurationHolder.config.grails.mime.types[params.format]
 			response.setHeader("Content-disposition", "attachment; filename=person.${params.extension}")
 
-			List fields = ["registrationDate", "registrationNumber","lastName","firstName","age","birthDate", "registrationAddress"]
-			Map labels = ["registrationDate": "Registration Date" ,"registrationNumber": "Registration Number", 
-                "firstName": "First Name", "lastName": "Last Name", 
-				"birthDate" : "DOB", "age" : "Age", "registrationAddress" : "Address"]
+			List fields  
+			Map labels 
+			Map parameters 
 
-			Map parameters = ["column.widths": [0.1,0.1,0.2, 0.2, 0.1, 0.1, 0.2]]
+            switch(params.format){
+                case "pdf":
+                    fields = pdfFields
+                    labels = pdfLabels
+                    parameters = pdfParams
+                    break
+
+                case "excel":
+                    fields = excelFields
+                    labels = excelLabels
+                    parameters = excelParams
+                    break
+            }
+
 
             def dateFormatter = {domain, value->
                 value.format("dd-MMM-yyyy")
@@ -36,10 +70,13 @@ class PersonController {
                 case "ALL":
 			        voters = voterService.listByDivision(divisionInstance)
                     parameters.title = "Voters"
+                    log.info "Printed list of all Voters as ${params.format}"
                     break
+
                 case "NAME":
                     voters = voterService.searchByDivision(params.searchString, divisionInstance,0 , 0)
                     parameters.title = "Voters with Name that Matches: ${params.searchString}"
+                    log.info "Printed voters list: Voters with Name that matches: ${params.searchString} as ${params.format} ."
                     break
 
                 case "AFFILIATION":
@@ -47,6 +84,7 @@ class PersonController {
                     def affiliationInstance = Affiliation.get(params.affiliation.toLong())
                     voters = voterService.filter(filterType.AFFILIATION,(Object)affiliationInstance,divisionInstance,0, 0)
                     parameters.title = "Voters with ${affiliationInstance} Affiliation"
+                    log.info "Printed voters list: Voters with ${affiliationInstance} Affiliation as ${params.format} ."
                     break
 
                 case "POLLSTATION":
@@ -54,21 +92,21 @@ class PersonController {
                     def pollStationInstance = PollStation.get(params.pollStation.toLong())
                     voters = voterService.filter(filterType.POLL_STATION,(Object)pollStationInstance,divisionInstance,0, 0)
                     parameters.title = "Voters at Poll Station # ${pollStationInstance} "
+                    log.info "Printed voters list : Voters at Poll Station # ${pollStationInstance} as ${params.format} ."
                     break
-
 
             }
 
 
 			exportService.export(params.format,response.outputStream,voters, 
 				fields, labels, formatters,parameters)
+
 		}
 	 }
 
 
 
 	 def export = {
-        println "\nlistType: ${params.listType}"
         def listType = params.listType
         def extension = (params.format == "pdf") ? "pdf" : "xls"
         def format = params.format
