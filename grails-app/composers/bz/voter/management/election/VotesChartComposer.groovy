92f6947b820ade358a4fab3d554817c87b2213ab
Roberto Guerra <uris77@gmail.com>
2012-02-03 05:38:16 -0600

    Issue #11 Can Display votes by Affiliation & Poll# A break down of total votes by affiliation is now displayed in table format and as a pie chart. ToDo: display a breakdown of votes by hour.

--- grails-app/composers/bz/voter/management/election/VotesChartComposer.groovy
+++ grails-app/composers/bz/voter/management/election/VotesChartComposer.groovy
@@ -3,6 +3,7 @@ package bz.voter.management.election
 import org.zkoss.zkgrails.*
 import org.zkoss.zk.ui.Executions
 import org.zkoss.zul.Hlayout
+import org.zkoss.zul.Messagebox
 
 import org.codehaus.groovy.grails.commons.ConfigurationHolder
 
@@ -14,46 +15,81 @@ import bz.voter.management.Election
 import bz.voter.management.Division
 import bz.voter.management.PollStation
 import bz.voter.management.Voter
+import bz.voter.management.zk.ComposerHelper
+
+import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
 
 class VotesChartComposer extends GrailsComposer {
 
     def chartBox
+    def tableBox
+    def votesSummaryGrid
+    def votesSummaryRows
     def votersChartPanel
+    def pollStationListbox
+    def votesBtn
 
     def chartsCount
-    def division
+    Division division
+    PollStation pollStation
+    Election election
+
+    Hlayout hlayout
 
     def afterCompose = { window ->
-        def electionId = Executions.getCurrent().getArg().electionId
+        if(SpringSecurityUtils.ifAnyGranted('ROLE_ADMIN, ROLE_OFFICE_STATION')){
+            def electionId = Executions.getCurrent().getArg().electionId
 
-        Election election = Election.get(electionId.toLong())
+            election = Election.get(electionId.toLong())
 
-        division = Division.findByName(ConfigurationHolder.config.division)
+            division = Division.findByName(ConfigurationHolder.config.division)
 
-        chartBox.getChildren().clear()
-
-
-        for(PollStation pollStation : PollStation.findAllByDivision(division)){
-            def results = VoterElection.getCountOfVotesByElectionAndPollStation(election, pollStation)
-            displayChart(pollStation,results)
+            chartBox.getChildren().clear()
+            for(PollStation pollStation : PollStation.findAllByDivision(division)){
+                pollStationListbox.append{
+                    listitem(value: pollStation, selected:false){
+                        listcell(label: pollStation.pollNumber)
+                        listcell(label: pollStation.id)
+                    }
+                }
+        }
+        }else{
+            ComposerHelper.permissionDeniedBox()
         }
 
+    }
 
+
+    def onClick_votesBtn(){
+        if(SpringSecurityUtils.ifAnyGranted('ROLE_ADMIN, ROLE_OFFICE_STATION')){
+            pollStation = pollStationListbox.getSelectedItem()?.getValue()
+            if(pollStation){
+                def results = VoterElection.getCountOfVotesByElectionAndPollStation(election, pollStation)
+                chartBox.getChildren().clear()
+                votesSummaryRows.getChildren().clear()
+                displayChart(pollStation, results)
+            }else{
+                Messagebox.show("Kindly Select a Poll Station!", "Charts Message", Messagebox.OK,
+                    Messagebox.EXCLAMATION)
+            }
+        }else{
+            ComposerHelper.permissionDeniedBox()
+        }
     }
 
     
 
     def displayChart(pollStation,results){
         H5Chart chart = new H5Chart()
-        chart.width = "300"
-        chart.height = "300"
+        chart.width = "180"
+        chart.height = "180"
         chart.setRegionManager(true)
 
         Pie pie = new Pie()
         pie.setLeft("20")
-        pie.setTop("4")
-        pie.setWidth("200")
-        pie.setHeight("200")
+        pie.setTop("2")
+        pie.setWidth("150")
+        pie.setHeight("150")
         pie.setAnimate(true)
         pie.setPizza(false)
         pie.setAnimateType(Pie.ANIMATION_RADIAL)
@@ -66,10 +102,46 @@ class VotesChartComposer extends GrailsComposer {
 
         chart.appendChild(pie)
         
-        Hlayout hlayout = new Hlayout()
+        hlayout = new Hlayout()
         chartBox.appendChild(hlayout)
 
         hlayout.appendChild(chart)
 
+        gridSetup()
+
     }
+
+
+    def gridSetup(){
+	    def allVoters = Voter.totalVotersByPollStation(pollStation)
+
+        def votesSummary = VoterElection.getCountOfVotesByElectionAndPollStation(election, pollStation)
+
+        def pollStationTotalVotes = 0
+
+        votesSummaryRows.append{
+            for(votes in votesSummary){
+                row{
+                    label(value: "${votes[1]}", style:"font-size: 0.75em")
+                    label(value: "${votes[0]}", style: "font-size: 0.75em")
+                    label(value: "${(votes[0]/allVoters) * 100} %", style:"font-size: 0.75em")
+                    label(value: "${allVoters}", style:"font-size: 0.75em")
+                }
+                pollStationTotalVotes += votes[0]
+            }
+            def percentOfVotersWhoVoted = (pollStationTotalVotes/allVoters)*100
+            row(style: 'background-color: khaki'){
+                label(value: "Total", style:"font-size: 0.75em")
+                label(value: "${pollStationTotalVotes}", style:"font-size: 0.75em")
+                label(value: "${percentOfVotersWhoVoted}", style:"font-size: 0.75em")
+                label(value: "${allVoters} %", style:"font-size: 0.75em")
+            }
+       }
+
+
+        votesSummaryGrid.visible = true
+
+
+    }
+
 }
